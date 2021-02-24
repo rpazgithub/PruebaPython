b6074f4cb220b356d75528878986a049d232a36e
Ruben Paz <rubenpaz0980@gmail.com>
2021-02-24 10:28:26 -0500

    Prueba django

---
+++ Cuenta.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProyectoCuenta
+{
+    class Cuenta
+    {
+
+        private decimal saldo;
+
+        public Cuenta(decimal saldoInicial)
+        {
+
+            Saldo = saldoInicial;
+                              
+        }
+
+        public decimal Saldo
+        {
+
+            set
+            {
+
+                if (value >= 0)
+                    saldo = value;
+
+            }
+
+            get
+            {
+
+                return saldo;
+
+            }
+
+        }
+
+        public void Acredita( decimal monto )
+        {
+
+            Saldo = Saldo + monto;
+
+        }
+
+    }
+}
---
+++ PruebaCuenta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ProyectoCuenta
+{
+    class PruebaCuenta
+    {
+        static void Main(string[] args)
+        {
+            Cuenta cuenta1 = new Cuenta( 50.00M );
+            Cuenta cuenta2 = new Cuenta(-7.53M);
+
+            Console.WriteLine( "Saldo de cuenta 1: {0:C}", cuenta1.Saldo );
+            Console.WriteLine( "Saldo de cuenta 2: {0:C}", cuenta2.Saldo );
+
+            decimal montoDeposito;
+
+            Console.Write( "Escriba el monto a depositar para la cuenta 1: " );
+            montoDeposito = Convert.ToDecimal( Console.ReadLine() );
+
+            Console.WriteLine( "Se sumo {0:C} al saldo de la cuenta 1", montoDeposito );
+            cuenta1.Acredita( montoDeposito );
+
+            Console.Write( "Escriba el monto a depositar para a cuenta 2: ");
+            montoDeposito = Convert.ToDecimal( Console.ReadLine() );
+
+            Console.WriteLine( "Se sumo {0:C} al saldo de la cuenta 2: ", montoDeposito );
+            cuenta2.Acredita( montoDeposito );
+
+            Console.WriteLine( "Saldo de cuenta 1: {0:C}", cuenta1.Saldo );
+            Console.WriteLine( "Saldo de cuenta 2: {0:C}", cuenta2.Saldo );
+
+
+
+        }
+    }
+}
